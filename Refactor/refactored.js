15a41a13ecfe15a973b6e3aea6516224448ae409
Manfredas Minikas <p998fmm@fspa.myntet.se>
2019-02-07 13:17:33 +0200

    Last commit, everythings done

--- Refactor/refactored.js
+++ Refactor/refactored.js
@@ -5,11 +5,11 @@ describe("To test danske bank page", function () {
     let wait = 5000;
     browser.url('https://danskebank.dk/privat');
     const okButton = browser.$('=OK');
-    okButton.waitForExist(wait);
+    okButton.waitForDisplayed(wait);
     okButton.click();
     browser.$('=Login').click();
     const usernameBox = browser.$('=username49');
-    usernameBox.waitForExist(wait);
+    usernameBox.waitForDisplayed(wait);
     usernameBox.setValue('Test');
     const passwordBox = browser.$('=password49');
     passwordBox.clearValue();
@@ -18,39 +18,33 @@ describe("To test danske bank page", function () {
     assert.equal(title, 'Logged');
   });
 
-  // this part left
-
-  it("Testcase 2", function () {
+  it("Checking if user successfully logged in:", function () {
     let wait = 5000;
     browser.url('https://danskebank.dk/privat');
-    browser.pause(5000);
-    browser.waitForElement('=OK');
-    browser.click('=OK');
-    browser.click('=Login');
-    browser.waitForElementPresent('=username', wait);
-    browser.clearValue('=username49');
-    browser.setValue('=username49', 'Test');
-    browser.waitForElementVisible('=password', wait);
-    browser.clearValue('=password49');
-    browser.setValue('=password49', 'Test');
-    browser.click('=Sign');
-    browser.pause(5000);
-    if (browser.getTitle == 'Logged') {
-      console.log('User logged')
-    } else if (browser.getTitle != 'Logged') {
-      console.log('User not logged')
-    }
+    const okButton = browser.$('=OK');
+    okButton.waitForDisplayed(wait);
+    okButton.click();
+    browser.$('=Login').click();
+    const usernameBox = browser.$('=username49');
+    usernameBox.waitForDisplayed(wait);
+    usernameBox.clearValue();
+    usernameBox.setValue('Test');
+    const passwordBox = browser.$('=password49');
+    passwordBox.waitForDisplayed(wait);
+    passwordBox.clearValue();
+    passwordBox.setValue('Test');
+    browser.$('=Sign').click();
+    const title = browser.getTitle()
+    assert.equal(title, 'Logged');
+  });
 
+  it("Navigate to my accounts:", function () {
     // navigate to my accounts
-    if (browser.element('=accounts').isExisting() = true) {
-      browser.element('=accounts').click()
-    }
-    browser.waitForElementPresent('=accounts');
-    Helper.GetAccounts().then(size
-      if (size > 0) {
-        console.log('Accounts exist')
-      });
-
-
+    const accounts = browser.$('=accounts');
+    accounts.waitForDisplayed(10000);
+    accounts.click();
+    // navigated, now you have to expect something to validate the test
+    const accountName = browser.$('=account49');
+    assert.equal(accountName.getText(), 'Test Test');
   });
 });
